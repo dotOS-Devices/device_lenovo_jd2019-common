e18be49504ee86c841ad1eb62e3f023712f7c90a
LuK1337 <priv.luk@gmail.com>
2021-03-27 17:42:18 +0100

    sdm710-common: livedisplay: Uprev to 2.1
    Change-Id: I98b2ea16ed342339ee652d5cb79dcfd22f8c0a0d

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.lenovo_sdm710"
+#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.lenovo_sdm710"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
@@ -22,8 +22,8 @@
 
 #include "SunlightEnhancement.h"
 
-using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
-using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
 
 int main() {
     android::sp<ISunlightEnhancement> sunlightEnhancement = new SunlightEnhancement();
