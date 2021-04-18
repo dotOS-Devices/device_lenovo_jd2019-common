3f81f4a4d4e8ed77bbb9695fa022bf09b91a930e
Giammarco Senatore <giammasena@gmail.com>
2021-04-18 21:55:23 +0800

    sdm710-common: import Gcam MGC_8.1.101_A9_PV0h_snap

--- sdm710.mk
+++ sdm710.mk
@@ -71,8 +71,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    Snap
+    GCamera
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/org.codeaurora.snapcam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.codeaurora.snapcam.xml
+	
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
