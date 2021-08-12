7fd653260b5ac0c8a84e0e27a7cbd93507c33493
Aman Singh <amankumarsingh1412@gmail.com>
2021-08-13 00:21:32 +0800

    sdm710-common: re-add livedisplay
    - from xiaomi-sdm845-common as well

--- sdm710.mk
+++ sdm710.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lenovo_sdm710
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
