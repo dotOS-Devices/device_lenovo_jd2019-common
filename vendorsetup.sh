6960598c322fa98da4624acbeface2bcd84ac872
HsyHusker <misterhusker19@gmail.com>
2021-08-14 20:58:18 +0800

    sdm710-common: fix dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-cp hardware/qcom-caf/sdm845/Android.bp hardware/qcom-caf/sdm845-pe/
-cp hardware/qcom-caf/sdm845/Android.mk hardware/qcom-caf/sdm845-pe/
+cp hardware/qcom-caf/sdm845/Android.bp hardware/qcom-caf/sdm845-lenovo/
+cp hardware/qcom-caf/sdm845/Android.mk hardware/qcom-caf/sdm845-lenovo/
