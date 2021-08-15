4d7ea9324e0c087c7788030bcbbc46aeeda1f3b1
HsyHusker <misterhusker19@gmail.com>
2021-08-16 02:24:23 +0800

    sdm710-common: Toggle Mobile data off by default on startup
    - Device heats up after booting up, so toggle it off by default
    - Disable haptic feedback and touch sounds by default as well

--- sdm710.mk
+++ sdm710.mk
@@ -223,6 +223,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Mobile data
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.android.mobiledata=false
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
